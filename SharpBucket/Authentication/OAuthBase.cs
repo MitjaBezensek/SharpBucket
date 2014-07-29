7cd8d1b93466f157281b19b44679dda40c47e6a1
Mitja Bezenšek <mitja.bezensek@istudio.si>
2014-07-29 12:05:54 +0200

    Resharper formatted.

--- SharpBucket/Authentication/OAuthBase.cs
+++ SharpBucket/Authentication/OAuthBase.cs
@@ -82,8 +82,8 @@ namespace SharpBucket.Authentication{
             /// <summary>
             /// Helper function to compute a hash value
             /// </summary>
-            /// <param name="hashAlgorithm">The hashing algoirhtm used. If that algorithm needs some initialization, like HMAC and its derivatives, they should be initialized prior to passing it to this function</param>
-            /// <param name="data">The data to hash</param>
+            /// <param Name="hashAlgorithm">The hashing algoirhtm used. If that algorithm needs some initialization, like HMAC and its derivatives, they should be initialized prior to passing it to this function</param>
+            /// <param Name="data">The data to hash</param>
             /// <returns>a Base64 string of the hash value</returns>
             private string ComputeHash(HashAlgorithm hashAlgorithm, string data){
                 if (hashAlgorithm == null){
@@ -103,8 +103,8 @@ namespace SharpBucket.Authentication{
             /// <summary>
             /// Internal function to cut out all non oauth query string parameters (all parameters not begining with "oauth_")
             /// </summary>
-            /// <param name="parameters">The query string part of the Url</param>
-            /// <returns>A list of QueryParameter each containing the parameter name and value</returns>
+            /// <param Name="parameters">The query string part of the Url</param>
+            /// <returns>A list of QueryParameter each containing the parameter Name and value</returns>
             private List<QueryParameter> GetQueryParameters(string parameters){
                 if (parameters.StartsWith("?")){
                     parameters = parameters.Remove(0, 1);
@@ -134,7 +134,7 @@ namespace SharpBucket.Authentication{
             /// This is a different Url Encode implementation since the default .NET one outputs the percent encoding in lower case.
             /// While this is not a problem with the percent encoding spec, it is used in upper case throughout OAuth
             /// </summary>
-            /// <param name="value">The value to Url encode</param>
+            /// <param Name="value">The value to Url encode</param>
             /// <returns>Returns a Url encoded string</returns>
             private string UrlEncode(string value){
                 var result = new StringBuilder();
@@ -154,7 +154,7 @@ namespace SharpBucket.Authentication{
             /// <summary>
             /// Normalizes the request parameters according to the spec
             /// </summary>
-            /// <param name="parameters">The list of parameters already sorted</param>
+            /// <param Name="parameters">The list of parameters already sorted</param>
             /// <returns>a string representing the normalized parameters</returns>
             private string NormalizeRequestParameters(IList<QueryParameter> parameters){
                 var sb = new StringBuilder();
@@ -173,16 +173,16 @@ namespace SharpBucket.Authentication{
             /// <summary>
             /// Generate the signature base that is used to produce the signature
             /// </summary>
-            /// <param name="url">The full url that needs to be signed including its non OAuth url parameters</param>
-            /// <param name="consumerKey">The consumer key</param>        
-            /// <param name="token">The token, if available. If not available pass null or an empty string</param>
-            /// <param name="tokenSecret">The token secret, if available. If not available pass null or an empty string</param>
-            /// <param name="httpMethod">The http method used. Must be a valid HTTP method verb (POST,GET,PUT, etc)</param>
-            /// <param name="nonce"></param>
-            /// <param name="signatureType">The signature type. To use the default values use <see cref="OAuthBase.SignatureTypes">OAuthBase.SignatureTypes</see>.</param>
-            /// <param name="timeStamp"></param>
-            /// <param name="normalizedUrl"></param>
-            /// <param name="normalizedRequestParameters"></param>
+            /// <param Name="url">The full url that needs to be signed including its non OAuth url parameters</param>
+            /// <param Name="consumerKey">The consumer key</param>        
+            /// <param Name="token">The token, if available. If not available pass null or an empty string</param>
+            /// <param Name="tokenSecret">The token secret, if available. If not available pass null or an empty string</param>
+            /// <param Name="httpMethod">The http method used. Must be a valid HTTP method verb (POST,GET,PUT, etc)</param>
+            /// <param Name="nonce"></param>
+            /// <param Name="signatureType">The signature type. To use the default values use <see cref="OAuthBase.SignatureTypes">OAuthBase.SignatureTypes</see>.</param>
+            /// <param Name="timeStamp"></param>
+            /// <param Name="normalizedUrl"></param>
+            /// <param Name="normalizedRequestParameters"></param>
             /// <returns>The signature base</returns>
             private string GenerateSignatureBase(Uri url, string consumerKey, string token, string tokenSecret, string httpMethod, string timeStamp, string nonce, string signatureType, out string normalizedUrl, out string normalizedRequestParameters){
                 if (token == null){
@@ -239,8 +239,8 @@ namespace SharpBucket.Authentication{
             /// <summary>
             /// Generate the signature value based on the given signature base and hash algorithm
             /// </summary>
-            /// <param name="signatureBase">The signature based as produced by the GenerateSignatureBase method or by any other means</param>
-            /// <param name="hash">The hash algorithm used to perform the hashing. If the hashing algorithm requires initialization or a key it should be set prior to calling this method</param>
+            /// <param Name="signatureBase">The signature based as produced by the GenerateSignatureBase method or by any other means</param>
+            /// <param Name="hash">The hash algorithm used to perform the hashing. If the hashing algorithm requires initialization or a key it should be set prior to calling this method</param>
             /// <returns>A base64 string of the hash value</returns>
             private string GenerateSignatureUsingHash(string signatureBase, HashAlgorithm hash){
                 return ComputeHash(hash, signatureBase);
@@ -249,16 +249,16 @@ namespace SharpBucket.Authentication{
             /// <summary>
             /// Generates a signature using the HMAC-SHA1 algorithm
             /// </summary>		
-            /// <param name="url">The full url that needs to be signed including its non OAuth url parameters</param>
-            /// <param name="consumerKey">The consumer key</param>
-            /// <param name="consumerSecret">The consumer seceret</param>
-            /// <param name="token">The token, if available. If not available pass null or an empty string</param>
-            /// <param name="tokenSecret">The token secret, if available. If not available pass null or an empty string</param>
-            /// <param name="httpMethod">The http method used. Must be a valid HTTP method verb (POST,GET,PUT, etc)</param>
-            /// <param name="timeStamp"></param>
-            /// <param name="nonce"></param>
-            /// <param name="normalizedUrl"></param>
-            /// <param name="normalizedRequestParameters"></param>
+            /// <param Name="url">The full url that needs to be signed including its non OAuth url parameters</param>
+            /// <param Name="consumerKey">The consumer key</param>
+            /// <param Name="consumerSecret">The consumer seceret</param>
+            /// <param Name="token">The token, if available. If not available pass null or an empty string</param>
+            /// <param Name="tokenSecret">The token secret, if available. If not available pass null or an empty string</param>
+            /// <param Name="httpMethod">The http method used. Must be a valid HTTP method verb (POST,GET,PUT, etc)</param>
+            /// <param Name="timeStamp"></param>
+            /// <param Name="nonce"></param>
+            /// <param Name="normalizedUrl"></param>
+            /// <param Name="normalizedRequestParameters"></param>
             /// <returns>A base64 string of the hash value</returns>
             public string GenerateSignature(Uri url, string consumerKey, string consumerSecret, string token, string tokenSecret, string httpMethod, string timeStamp, string nonce, out string normalizedUrl, out string normalizedRequestParameters){
                 return GenerateSignature(url, consumerKey, consumerSecret, token, tokenSecret, httpMethod, timeStamp, nonce, SignatureTypes.HMACSHA1, out normalizedUrl, out normalizedRequestParameters);
@@ -267,17 +267,17 @@ namespace SharpBucket.Authentication{
             /// <summary>
             /// Generates a signature using the specified signatureType 
             /// </summary>		
-            /// <param name="url">The full url that needs to be signed including its non OAuth url parameters</param>
-            /// <param name="consumerKey">The consumer key</param>
-            /// <param name="consumerSecret">The consumer seceret</param>
-            /// <param name="token">The token, if available. If not available pass null or an empty string</param>
-            /// <param name="tokenSecret">The token secret, if available. If not available pass null or an empty string</param>
-            /// <param name="httpMethod">The http method used. Must be a valid HTTP method verb (POST,GET,PUT, etc)</param>
-            /// <param name="nonce"></param>
-            /// <param name="signatureType">The type of signature to use</param>
-            /// <param name="timeStamp"></param>
-            /// <param name="normalizedUrl"></param>
-            /// <param name="normalizedRequestParameters"></param>
+            /// <param Name="url">The full url that needs to be signed including its non OAuth url parameters</param>
+            /// <param Name="consumerKey">The consumer key</param>
+            /// <param Name="consumerSecret">The consumer seceret</param>
+            /// <param Name="token">The token, if available. If not available pass null or an empty string</param>
+            /// <param Name="tokenSecret">The token secret, if available. If not available pass null or an empty string</param>
+            /// <param Name="httpMethod">The http method used. Must be a valid HTTP method verb (POST,GET,PUT, etc)</param>
+            /// <param Name="nonce"></param>
+            /// <param Name="signatureType">The type of signature to use</param>
+            /// <param Name="timeStamp"></param>
+            /// <param Name="normalizedUrl"></param>
+            /// <param Name="normalizedRequestParameters"></param>
             /// <returns>A base64 string of the hash value</returns>
             private string GenerateSignature(Uri url, string consumerKey, string consumerSecret, string token, string tokenSecret, string httpMethod, string timeStamp, string nonce, SignatureTypes signatureType, out string normalizedUrl, out string normalizedRequestParameters){
                 normalizedUrl = null;
