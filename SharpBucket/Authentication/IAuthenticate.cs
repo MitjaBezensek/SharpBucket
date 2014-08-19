5b512b9bcaf16ff8a338e4c0d2e53ed0498d1e9f
Mitja Bezenšek <mitja.bezensek@istudio.si>
2014-08-19 19:41:26 +0200

    Fixed a type with ExecuteRequest method name

--- SharpBucket/Authentication/IAuthenticate.cs
+++ SharpBucket/Authentication/IAuthenticate.cs
@@ -4,7 +4,7 @@ namespace SharpBucket.Authentication{
     public abstract class Authenticate{
         protected RestClient client;
         public virtual T GetResponse<T>(string url, Method method, T body) {
-            var executeMethod = typeof(RequestExcecutor).GetMethod("ExectueRequest");
+            var executeMethod = typeof(RequestExcecutor).GetMethod("ExecuteRequest");
             var generic = executeMethod.MakeGenericMethod(typeof(T));
             return (T)generic.Invoke(this, new object[] { url, method, body, client });
         }
--- SharpBucket/Authentication/RequestExcecutor.cs
+++ SharpBucket/Authentication/RequestExcecutor.cs
@@ -5,7 +5,7 @@ namespace SharpBucket.Authentication{
     internal class RequestExcecutor{
         private static readonly LowerCaseSerializer serializer = new LowerCaseSerializer();
 
-        public static T ExectueRequest<T>(string url, Method method, T body, RestClient client) where T : new(){
+        public static T ExecuteRequest<T>(string url, Method method, T body, RestClient client) where T : new(){
             client.AddHandler("application/json", serializer);
             var request = new RestRequest(url, method);
             if (ShouldAddBody(method)){
