0729328a15a67d98ba1890e1a78c2cbfb5a22d54
Mitja Bezenšek <mitja.bezensek@istudio.si>
2014-08-19 21:20:08 +0200

    Added documentation for the UserEndPoint

--- SharpBucket/V1/EndPoints/UserEndPointV1.cs
+++ SharpBucket/V1/EndPoints/UserEndPointV1.cs
@@ -19,27 +19,48 @@ namespace SharpBucket.V1.EndPoints{
             return _sharpBucketV1.Get(new UserInfo(), _baseUrl);
         }
 
+        /// <summary>
+        /// List the privileges for the current user.
+        /// </summary>
+        /// <returns></returns>
         public Privileges ListPrivileges(){
             var overrideUrl = _baseUrl + "privileges";
             return _sharpBucketV1.Get(new Privileges(), overrideUrl);
         }
 
+        /// <summary>
+        /// List the repositories that the current user follows.
+        /// </summary>
+        /// <returns></returns>
         public List<Repository> ListFollows(){
             var overrideUrl = _baseUrl + "follows";
             return _sharpBucketV1.Get(new List<Repository>(), overrideUrl);
         }
 
+        /// <summary>
+        /// List the repositories of the current user.
+        /// </summary>
+        /// <returns></returns>
         public List<Repository> ListRepositories(){
             var overrideUrl = _baseUrl + "repositories";
             return _sharpBucketV1.Get(new List<Repository>(), overrideUrl);
         }
 
+        /// <summary>
+        /// Get the overview of the repositories for the current user.
+        /// </summary>
+        /// <returns></returns>
         public RepositoriesOverview RepositoriesOverview(){
             var overrideUrl = _baseUrl + "repositories/overview";
             return _sharpBucketV1.Get(new RepositoriesOverview(), overrideUrl);
         }
 
+        
         // TODO: Fix serialization
+        /// <summary>
+        /// Get the repository dashboard for the current user.
+        /// </summary>
+        /// <returns></returns>
         public object GetRepositoryDasboard(){
             var overrideUrl = _baseUrl + "repositories/dashboard";
             return _sharpBucketV1.Get(new object(), overrideUrl);
