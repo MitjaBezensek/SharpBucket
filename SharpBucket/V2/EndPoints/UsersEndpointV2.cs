ae8a301948ba7e336afda1370267b0c5459a1ced
Mitja Bezenšek <mitja.bezensek@istudio.si>
2014-08-19 23:28:25 +0200

    Added UsersEndPointDocumentation.

--- SharpBucket/V2/EndPoints/UsersEndpointV2.cs
+++ SharpBucket/V2/EndPoints/UsersEndpointV2.cs
@@ -2,6 +2,11 @@
 using SharpBucket.V2.Pocos;
 
 namespace SharpBucket.V2.EndPoints{
+    /// <summary>
+    /// The Users End Point gets a user account's profile information.
+    /// More info:
+    /// https://confluence.atlassian.com/display/BITBUCKET/users+Endpoint
+    /// </summary>
     public class UsersEndpointV2{
         private readonly SharpBucketV2 _sharpBucketV2;
 
@@ -14,21 +19,39 @@ namespace SharpBucket.V2.EndPoints{
             _repositoriesUrl = "repositories/" + accountName + "/";
         }
 
+        /// <summary>
+        /// Gets the public information associated with a user. 
+        /// If the user's profile is private, the caller must be authenticated as the account holder to view this information.  
+        /// </summary>
+        /// <returns></returns>
         public User GetProfile(){
             return _sharpBucketV2.Get(new User(), _baseUrl);
         }
 
+        /// <summary>
+        /// List all the accounts following the user.  
+        /// </summary>
+        /// <returns></returns>
         public UsersInfo ListFollowers(){
             var overrideUrl = _baseUrl + "followers/";
             return _sharpBucketV2.Get(new UsersInfo(), overrideUrl);
         }
 
+        /// <summary>
+        /// List all the accounts the user is following. 
+        /// </summary>
+        /// <returns></returns>
         public UsersInfo ListFollowing(){
             var overrideUrl = _baseUrl + "following/";
             return _sharpBucketV2.Get(new UsersInfo(), overrideUrl);
         }
 
-        // Moved permanently
+        // TODO: Moved permanently
+        /// <summary>
+        /// List all of the user's repositories. 
+        /// Private repositories only appear on this list if the caller is authenticated and is authorized to view the repository.
+        /// </summary>
+        /// <returns></returns>
         public List<Repository> ListRepositories(){
             return _sharpBucketV2.Get(new List<Repository>(), _repositoriesUrl);
         }
