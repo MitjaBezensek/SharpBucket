04882e5de160fd88ab636ce6eca310ba3f883681
Mitja Bezenšek <mitja.bezensek@istudio.si>
2014-08-19 23:25:44 +0200

    Added TeamsEndPoint documentation

--- SharpBucket/V2/EndPoints/TeamsEndPointV2.cs
+++ SharpBucket/V2/EndPoints/TeamsEndPointV2.cs
@@ -2,6 +2,11 @@
 using SharpBucket.V2.Pocos;
 
 namespace SharpBucket.V2.EndPoints{
+    /// <summary>
+    /// The /teams endpoint gets a team's profile information.
+    /// More info:
+    /// https://confluence.atlassian.com/display/BITBUCKET/teams+Endpoint
+    /// </summary>
     public class TeamsEndPointV2{
         private readonly SharpBucketV2 _sharpBucketV2;
         private readonly string _baseUrl;
@@ -11,25 +16,47 @@ namespace SharpBucket.V2.EndPoints{
             _baseUrl = "teams/" + teamName + "/";
         }
 
+        /// <summary>
+        /// Gets the public information associated with a team. 
+        /// If the team's profile is private, the caller must be authenticated and authorized to view this information. 
+        /// </summary>
+        /// <returns></returns>
         public Team GetProfile(){
             return _sharpBucketV2.Get(new Team(), _baseUrl);
         }
 
+        /// <summary>
+        /// Gets the team's members. By default, this call returns the first 50 members of the team. 
+        /// </summary>
+        /// <returns></returns>
         public User ListMembers(){
             var overrideUrl = _baseUrl + "members/";
             return _sharpBucketV2.Get(new User(), overrideUrl);
         }
 
+        /// <summary>
+        /// Gets the list of accounts following the team.
+        /// </summary>
+        /// <returns></returns>
         public TeamProfile ListFollowers(){
             var overrideUrl = _baseUrl + "followers/";
             return _sharpBucketV2.Get(new TeamProfile(), overrideUrl);
         }
 
+        /// <summary>
+        /// Gets a list of accounts the team is following. 
+        /// </summary>
+        /// <returns></returns>
         public TeamProfile ListFollowing(){
             var overrideUrl = _baseUrl + "following/";
             return _sharpBucketV2.Get(new TeamProfile(), overrideUrl);
         }
 
+        /// <summary>
+        /// Gets the list of the team's repositories. 
+        /// Private repositories only appear on this list if the caller is authenticated and is authorized to view the repository.
+        /// </summary>
+        /// <returns></returns>
         public List<Repository> ListRepositories(){
             var overrideUrl = _baseUrl + "repositories/";
             return _sharpBucketV2.Get(new List<Repository>(), overrideUrl);
