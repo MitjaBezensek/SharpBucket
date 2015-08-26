276a45f3c8bf556e6ab6bf9007b50a78e8a46b4b
Mitja Bezenšek <mitja.bezensek@gmail.com>
2015-08-26 21:25:17 +0200

    Add missing test to repositories end point

--- SharpBucketTests/V2/EndPoints/RepositoriesEndPointTests.cs
+++ SharpBucketTests/V2/EndPoints/RepositoriesEndPointTests.cs
@@ -1,25 +1,34 @@
 ﻿using NUnit.Framework;
 using SharpBucket.V2;
+using SharpBucket.V2.EndPoints;
 using Shouldly;
 
 namespace SharBucketTests.V2.EndPoints{
    [TestFixture]
    internal class RepositoriesEndPointTests{
       private SharpBucketV2 sharpBucket;
+      private RepositoriesEndPoint repositoriesEndPoint;
 
       [SetUp]
       public void Init(){
          sharpBucket = TestHelpers.GetV2ClientAuthenticatedWithBasicAuthentication();
+         repositoriesEndPoint = sharpBucket.RepositoriesEndPoint();
       }
 
       [Test]
       public void ListRepositories_WithNoMaxSet_ReturnsAtLeast10Repositories(){
-         var repositoriesEndPoint = sharpBucket.RepositoriesEndPoint();
          repositoriesEndPoint.ShouldNotBe(null);
 
          var repositories = repositoriesEndPoint.ListRepositories("mirror");
          repositories.ShouldNotBe(null);
          repositories.Count.ShouldBeGreaterThan(10);
+      }
+
+      [Test]
+      public void ListPublicRepositories_With30AsMax_Returns30PublicRepositories(){
+         var publicRepositories = repositoriesEndPoint.ListPublicRepositories(30);
+         publicRepositories.Count.ShouldBe(30);
+         publicRepositories[5].full_name.ShouldBe("vetler/fhtmlmps");
       }
    }
 }
