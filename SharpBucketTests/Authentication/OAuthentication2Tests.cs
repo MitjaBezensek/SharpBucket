fca3b7942448bfd8f81622c143d72625fa2323d0
Mitja Bezenšek <mitja.bezensek@gmail.com>
2015-10-17 09:19:12 +0200

    Refactor the name of OAuth2 test

--- SharpBucketTests/Authentication/OAuthentication2Tests.cs
+++ SharpBucketTests/Authentication/OAuthentication2Tests.cs
@@ -5,7 +5,7 @@ namespace SharBucketTests.Authentication {
     [TestFixture]
     internal class OAuthentication2Tests {
         [Test]
-        public void Test() {
+        public void OAuth2_RequestWithParameters_GetsPublicRepositories() {
             var sharpbucket = TestHelpers.GetV2ClientAuthenticatedWithOAuth2();
             var publicRepos = sharpbucket.RepositoriesEndPoint().ListPublicRepositories(max: 50);
             publicRepos.ShouldNotBe(null);
