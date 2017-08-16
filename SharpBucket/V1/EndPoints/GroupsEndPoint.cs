699b220b6d8567b62817734042d5ed8a452f0b37
Maxwell Obi <obi.maxwell@rocketmail.com>
2017-08-16 22:20:31 +0100

    Created methods for the groups endpoint

---
+++ SharpBucket/V1/EndPoints/GroupsEndPoint.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using SharpBucket.V1.Pocos;
+
+namespace SharpBucket.V1.EndPoints
+{
+    /// <summary>
+    /// The groups endpoint provides functionality for querying information about user groups, 
+    /// creating new ones, updating memberships, and deleting them. Both individual and team accounts can define groups.
+    /// To manage group information on an individual account, the caller must authenticate with administrative rights on the account. 
+    /// More info here:
+    /// https://confluence.atlassian.com/bitbucket/groups-endpoint-296093143.html
+    /// </summary>
+
+    public class GroupsEndPoint {
+        private readonly string _baseUrl;
+        private readonly SharpBucketV1 _sharpBucketV1;
+
+        public GroupsEndPoint(string accountName, SharpBucketV1 sharpBucketV1) {
+            _sharpBucketV1 = sharpBucketV1;
+            _baseUrl = $"groups/{accountName}/";
+        }
+
+        /// <summary>
+        /// Get a list of all groups in an account. 
+        /// </summary>
+        /// <returns></returns>
+        public List<Group> ListGroups() {
+            return _sharpBucketV1.Get<List<Group>>(new List<Group>(), _baseUrl);
+        }
+
+        /// <summary>
+        /// Get the details of a group. 
+        /// </summary>
+        /// <param name="group_slug">The group's slug.</param>
+        /// <returns></returns>
+        public Group GetGroup(string group_slug) {
+            var overrideUrl = _baseUrl + group_slug;
+            return _sharpBucketV1.Get<Group>(new Group(), overrideUrl);
+        }
+
+        /// <summary>
+        /// Creates a new default group 
+        /// </summary>
+        /// <param name="name">The name of the group</param>
+        /// <returns></returns>
+        public Group CreateGroup(string name) {
+
+            return null;
+        }
+
+        /// <summary>
+        /// Deletes the specified group
+        /// <param name="group_slug">The group's slug.</param>
+        /// <returns></returns>
+        public Group DeleteGroup(string group_slug) {
+
+            return null;
+        }
+
+        /// <summary>
+        /// Updates the specified group
+        /// <param name="group_slug">The group's slug.</param>
+        /// <returns></returns>
+        public Group UpdateGroup(string group_slug) {
+
+            return null;
+        }
+
+        /// <summary>
+        /// Adds a member to a group
+        /// </summary>
+        /// <param name="group_slug">The group's slug</param>
+        /// <param name="membername">An individual account name. This can be an account name or the primary email address for the account</param>
+        /// <returns></returns>
+        public User AddMemberToGroup(string group_slug, string membername) {
+            var overrideUrl = _baseUrl + group_slug + "/members/" + membername;
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the membership for a group 
+        /// </summary>
+        /// <param name="group_slug">The group's slug.</param>
+        /// <returns></returns>
+        public List<User> ListGroupMembers(string group_slug) {
+            var overrideUrl = _baseUrl + group_slug + "/members";
+            return _sharpBucketV1.Get<List<User>>(new List<User>(), overrideUrl);
+        }
+
+        /// <summary>
+        /// Adds a member to a group
+        /// </summary>
+        /// <param name="group_slug">The group's slug</param>
+        /// <param name="membername">An individual account name. This can be an account name or the primary email address for the account</param>
+        /// <returns></returns>
+        public User DeleteMemberFromGroup(string group_slug, string membername) {
+            var overrideUrl = _baseUrl + group_slug + "/members/" + membername;
+
+            return null;
+        }
+    }
+}
