7e6b38e25d29d2e4c388473d45a272f0151c0583
Maxwell Obi <obi.maxwell@rocketmail.com>
2017-08-16 22:24:53 +0100

    Created V1 tests for the groups endpoint

---
+++ SharpBucketTests/V1/TestHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SharpBucket.V1;
+
+namespace SharBucketTests{
+    internal partial class TestHelpers{
+       
+        public static SharpBucketV1 GetV1ClientAuthenticatedWithOAuth(){
+
+            //get the environment variable from OS registry key for the current user
+            var consumerKey = Environment.GetEnvironmentVariable(SbConsumerKey, EnvironmentVariableTarget.User);
+            var consumerSecretKey = Environment.GetEnvironmentVariable(SbConsumerSecretKey, EnvironmentVariableTarget.User);
+
+            var sharpbucket = new SharpBucketV1();
+            sharpbucket.OAuth2LeggedAuthentication(consumerKey, consumerSecretKey);
+            return sharpbucket;
+        }
+
+    }
+}
