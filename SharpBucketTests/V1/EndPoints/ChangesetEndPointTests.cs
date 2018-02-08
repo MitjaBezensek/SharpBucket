7f084ad9b74d8ca9bb7db566479775506d547089
Nick McKenzie <nickm@nvisionit.co.za>
2018-02-08 07:57:08 +0200

    Correct environment variable

--- SharpBucketTests/V1/EndPoints/ChangesetEndPointTests.cs
+++ SharpBucketTests/V1/EndPoints/ChangesetEndPointTests.cs
@@ -19,15 +19,10 @@ namespace SharBucketTests.V1.EndPoints
         [SetUp]
         public void Init()
         {
-            Environment.SetEnvironmentVariable("SB_ACCOUNT_NAME", "mirror");
-            Environment.SetEnvironmentVariable("SB_CONSUMER_SECRET_KEY", "yedGsmuE6q4gzh3rqppvE66mfSqnEAKg");
-            Environment.SetEnvironmentVariable("SB_CONSUMER_KEY", "5Y6cBVjaunnx5cS65S");
-
             sharpBucket = TestHelpers.GetV1ClientAuthenticatedWithOAuth();
-            accountName = TestHelpers.GetAccountName();
+            accountName = "mirror";
             repositoriesEndPoint = sharpBucket.RepositoriesEndPoint(accountName, REPOSITORY_NAME);
         }
-
 
         [Test]
         public void GetChangesetDiffstat_when_limit_is_specified_should_return_that_number_of_diffsets()
