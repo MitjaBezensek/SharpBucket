9c0c7a8c74ade618a2f2d598cd66e60227ad581f
Anh-Duc Le <Anh-Duc_Le@outlook.com>
2018-03-07 12:14:04 +0100

    small wording change

--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -116,7 +116,7 @@ namespace ConsoleTests
         {
             var issuesResource = sharpBucket.RepositoriesEndPoint(accountName, repository).IssuesResource();
 
-            // You need a fifth issue in the repository.
+            // You need at least 5 issues in the repository.
             int ISSUE_ID = 5;
             // Issues
             var issues = issuesResource.ListIssues();
