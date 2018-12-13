c4c1994cd360083b0f334a6ccd81e42359e83309
Mickaël Nivet <mnivet@talentsoft.com>
2018-12-13 14:37:15 +0100

    Implement the OAuth2 authentication on BitBucket for LibGit2Sharp
    Some code has been shared with the OAuth2 authentication on BitBucket for APIs
    It's probably possible to share more code and improve OAuth2 support by implementing ReFreshToken behaviour, but it's not the goal right now.
    
    So we will kept all that code internal until we have a good code coverage and be able to made significant improvements with the tests as a safe net.

---
+++ SharpBucket/Authentication/OAuth2TokenProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace SharpBucket.Authentication
+{
+    /// <summary>
+    /// https://developer.atlassian.com/bitbucket/api/2/reference/meta/authentication#oauth-2
+    /// Client Credentials Grant (4.4)
+    /// </summary>
+    internal class OAuth2TokenProvider
+    {
+        private const string TokenUrl = "https://bitbucket.org/site/oauth2/access_token";
+
+        public string ConsumerKey { get; }
+
+        public string ConsumerSecret { get; }
+
+        public OAuth2TokenProvider(string consumerKey, string consumerSecret)
+        {
+            this.ConsumerKey = consumerKey;
+            this.ConsumerSecret = consumerSecret;
+        }
+
+        public Token GetToken()
+        {
+            var tempClient = new RestClient(TokenUrl)
+            {
+                Authenticator = new HttpBasicAuthenticator(ConsumerKey, ConsumerSecret),
+            };
+            var request = new RestRequest
+            {
+                Method = Method.POST
+            };
+            request.AddParameter("grant_type", "client_credentials");
+            var response = tempClient.Execute<Token>(request);
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception($"Unable to retrieve OAuth2 Token:{Environment.NewLine}{response.StatusCode}{Environment.NewLine}{response.Content}");
+            }
+
+            return response.Data;
+        }
+    }
+}
--- SharpBucket/Properties/AssemblyInfo.cs
+++ SharpBucket/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -33,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.7.3.0")]
 [assembly: AssemblyFileVersion("0.7.3.0")]
+
+[assembly: InternalsVisibleTo("SharpBucketTests")]
