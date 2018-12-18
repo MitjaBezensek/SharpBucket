b0a399e38fef0df38adbb2c090149447e535c36a
Mickaël Nivet <mnivet@talentsoft.com>
2018-12-18 17:15:59 +0100

    RequestExecutor is now split in two (RequestExecutorV1 and RequestExecutorV2) to distinct the way that objects should be serialized in the body of the post requests between API V1 and API V2
    I've tried to keep things internal as much as possible, but it has been required to perform few breaking changes
    BREAKING CHANGES:
    * In class Authenticate, the GetResponse method signature has slightly changed
      T GetResponse<T>(string url, Method method, T body, IDictionary<string, object> requestParameters)
      T GetResponse<T>(string url, Method method, object body, IDictionary<string, object> requestParameters) where T : new()
      Even if it's public it's not expected to be used directly so it should not really impact anyone
    * In class SharpBucket, the class is now abstract and the default constructor is no more
      By design, it was already a class which was useless alone so it should not really impact anyone

---
+++ SharpBucket/V2/RequestExecutorV2.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using RestSharp;
+using SharpBucket.Authentication;
+
+namespace SharpBucket.V2
+{
+    internal class RequestExecutorV2 : RequestExecutor
+    {
+        protected override void AddBody(RestRequest request, object body)
+        {
+            // Use Newtonsoft.Json serialization instead of the one included in RestSharp to be able to
+            // ignore null properties during the serialization
+            // https://stackoverflow.com/questions/20006813/restsharp-how-to-skip-serializing-null-values-to-json
+            var jsonSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+            var jsonString = JsonConvert.SerializeObject(body, jsonSettings);
+            request.AddParameter("application/json", jsonString, ParameterType.RequestBody);
+        }
+    }
+}
