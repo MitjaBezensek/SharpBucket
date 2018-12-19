72f7f2cd86a01d7b352c4620f38b99e147010d53
Aaron Sherber <aaron@sherber.com>
2018-12-19 18:09:19 -0500

    Refactor ToSlug() so that we can use UUIDs elsewhere

--- SharpBucket/Utility/StringExtensions.cs
+++ SharpBucket/Utility/StringExtensions.cs
@@ -11,18 +11,52 @@ namespace SharpBucket.Utility
         /// If a UUID in B or D format is passed in, it will be returned in B format.
         /// </summary>
         /// <param name="repoName">The name of a repository.</param>
-        /// <returns></returns>
+        /// <returns>The slug version of the repository name, or the formatted UUID.</returns>
         public static string ToSlug(this string repoName)
         {
-            if (Guid.TryParseExact(repoName, "B", out _))
-                return repoName;
-            if (Guid.TryParseExact(repoName, "D", out _))
-                return $"{{{repoName}}}";
+            if (repoName.TryGetGuid(out var guidString))
+                return guidString;
 
             if (repoName.Contains('\''))
                 repoName = repoName.Replace("'", "");
             var slugRegex = new Regex(@"[^a-zA-Z\.\-_0-9]+|-{2,}");
             return slugRegex.Replace(repoName, "-").ToLowerInvariant();
+        }
+
+        /// <summary>
+        /// Determines whether the input string is a Guid in B or D format.
+        /// </summary>
+        /// <param name="input">The string to test.</param>
+        /// <param name="guidString">The Guid in B format, or null.</param>
+        /// <returns>True if the input string is a Guid in B or D format, or false.</returns>
+        public static bool TryGetGuid(this string input, out string guidString)
+        {
+            if (Guid.TryParseExact(input, "B", out _))
+            {
+                guidString = input;
+                return true;
+            }
+            else if (Guid.TryParseExact(input, "D", out _))
+            {
+                guidString = $"{{{input}}}";
+                return true;
+            }
+            else
+            {
+                guidString = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns a formatted Guid if the input string is a Guid in B or D format,
+        /// or the input string if not.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string GuidOrValue(this string input)
+        {
+            return input.TryGetGuid(out var guidString) ? guidString : input;
         }
     }
 }
--- SharpBucketTests/V2/Utility/StringExtensionsTests.cs
+++ SharpBucketTests/V2/Utility/StringExtensionsTests.cs
@@ -22,5 +22,30 @@ namespace SharpBucketTests.V2.Utility
             var output = input.ToSlug();
             output.ShouldBe(expected);
         }
+
+        [TestCase("d12fbb96-2a37-4c1d-adf8-8d5da44d8f40", true, "{d12fbb96-2a37-4c1d-adf8-8d5da44d8f40}")]
+        [TestCase("{d12fbb96-2a37-4c1d-adf8-8d5da44d8f40}", true, "{d12fbb96-2a37-4c1d-adf8-8d5da44d8f40}")]
+        [TestCase("d12fbb962a374c1dadf88d5da44d8f40", false, null)]
+        [TestCase("foo", false, null)]
+        [TestCase("", false, null)]
+        [TestCase(null, false, null)]
+        public void TryGetGuid_ShouldWork(string input, bool expectedResult, string expectedOutput)
+        {
+            var result = input.TryGetGuid(out var output);
+            result.ShouldBe(expectedResult);
+            output.ShouldBe(expectedOutput);
+        }
+
+        [TestCase("ab806147-1e17-4e8d-b350-9b7bac807ef0", "{ab806147-1e17-4e8d-b350-9b7bac807ef0}")]
+        [TestCase("{ab806147-1e17-4e8d-b350-9b7bac807ef0}", "{ab806147-1e17-4e8d-b350-9b7bac807ef0}")]
+        [TestCase("ab8061471e174e8db3509b7bac807ef0", "ab8061471e174e8db3509b7bac807ef0")]
+        [TestCase("foo", "foo")]
+        [TestCase("", "")]
+        [TestCase(null, null)]
+        public void GuidOrValue_ShouldWork(string input, string expected)
+        {
+            var output = input.GuidOrValue();
+            output.ShouldBe(expected);
+        }
     }
 }
