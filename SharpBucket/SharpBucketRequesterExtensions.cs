46616553b89da90f10aa355648b1acf30fb94048
Mickael NIVET <mnivet@talentsoft.com>
2019-11-29 10:24:56 +0100

    Refactor SharpBucketRequesterExtensions to make CancellationToken parameters non optional. The goal is, like on master, to not forget to expose the CancellationToken parameter in functional public methods.
    Also remove the async/await useless keyworks for that basic wrapper methods
    see: https://stackoverflow.com/questions/35987375/always-use-the-async-and-await-keywords-in-asynchronous-methods-in-a-library

--- SharpBucket/SharpBucketRequesterExtensions.cs
+++ SharpBucket/SharpBucketRequesterExtensions.cs
@@ -6,26 +6,48 @@ namespace SharpBucket
 {
     public static class SharpBucketRequesterExtensions
     {
-        public static string Get(this ISharpBucketRequester requester, string relativeUrl, object requestParameters = null)
+        public static string Get(this ISharpBucketRequester requester, string relativeUrl)
+        {
+            return requester.Send(null, HttpMethod.Get, relativeUrl, null);
+        }
+
+        public static string Get(this ISharpBucketRequester requester, string relativeUrl, object requestParameters)
         {
             return requester.Send(null, HttpMethod.Get, relativeUrl, requestParameters);
         }
 
-        public static async Task<string> GetAsync(this ISharpBucketRequester requester, string relativeUrl, object requestParameters = null, CancellationToken token = default)
+        public static Task<string> GetAsync(this ISharpBucketRequester requester, string relativeUrl, CancellationToken token)
         {
-            return await requester.SendAsync(null, HttpMethod.Get, relativeUrl, requestParameters, token);
+            return requester.SendAsync(null, HttpMethod.Get, relativeUrl, null, token);
         }
 
-        public static T Get<T>(this ISharpBucketRequester requester, string relativeUrl, object requestParameters = null)
+        public static Task<string> GetAsync(this ISharpBucketRequester requester, string relativeUrl, object requestParameters, CancellationToken token)
+        {
+            return requester.SendAsync(null, HttpMethod.Get, relativeUrl, requestParameters, token);
+        }
+
+        public static T Get<T>(this ISharpBucketRequester requester, string relativeUrl)
+            where T : new()
+        {
+            return requester.Send<T>(null, HttpMethod.Get, relativeUrl, null);
+        }
+
+        public static T Get<T>(this ISharpBucketRequester requester, string relativeUrl, object requestParameters)
             where T : new()
         {
             return requester.Send<T>(null, HttpMethod.Get, relativeUrl, requestParameters);
         }
 
-        public static async Task<T> GetAsync<T>(this ISharpBucketRequester requester, string relativeUrl, object requestParameters = null, CancellationToken token = default)
+        public static Task<T> GetAsync<T>(this ISharpBucketRequester requester, string relativeUrl, CancellationToken token)
             where T : new()
         {
-            return await requester.SendAsync<T>(null, HttpMethod.Get, relativeUrl, requestParameters, token);
+            return requester.SendAsync<T>(null, HttpMethod.Get, relativeUrl, null, token);
+        }
+
+        public static Task<T> GetAsync<T>(this ISharpBucketRequester requester, string relativeUrl, object requestParameters, CancellationToken token)
+            where T : new()
+        {
+            return requester.SendAsync<T>(null, HttpMethod.Get, relativeUrl, requestParameters, token);
         }
 
         public static T Post<T>(this ISharpBucketRequester requester, T body, string relativeUrl)
@@ -34,10 +56,10 @@ namespace SharpBucket
             return requester.Send<T>(body, HttpMethod.Post, relativeUrl);
         }
 
-        public static async Task<T> PostAsync<T>(this ISharpBucketRequester requester, T body, string relativeUrl = null, CancellationToken token = default)
+        public static Task<T> PostAsync<T>(this ISharpBucketRequester requester, T body, string relativeUrl, CancellationToken token)
             where T : new()
         {
-            return await requester.SendAsync<T>(body, HttpMethod.Post, relativeUrl, null, token);
+            return requester.SendAsync<T>(body, HttpMethod.Post, relativeUrl, null, token);
         }
 
         public static T Put<T>(this ISharpBucketRequester requester, T body, string relativeUrl)
@@ -46,10 +68,10 @@ namespace SharpBucket
             return requester.Send<T>(body, HttpMethod.Put, relativeUrl);
         }
 
-        public static async Task<T> PutAsync<T>(this ISharpBucketRequester requester, T body, string relativeUrl = null, CancellationToken token = default)
+        public static Task<T> PutAsync<T>(this ISharpBucketRequester requester, T body, string relativeUrl, CancellationToken token)
             where T : new()
         {
-            return await requester.SendAsync<T>(body, HttpMethod.Put, relativeUrl, null, token);
+            return requester.SendAsync<T>(body, HttpMethod.Put, relativeUrl, null, token);
         }
 
         public static void Delete(this ISharpBucketRequester requester, string relativeUrl)
@@ -57,9 +79,9 @@ namespace SharpBucket
             requester.Send(null, HttpMethod.Delete, relativeUrl);
         }
 
-        public static async Task DeleteAsync(this ISharpBucketRequester requester, string relativeUrl = null, CancellationToken token = default)
+        public static Task DeleteAsync(this ISharpBucketRequester requester, string relativeUrl, CancellationToken token)
         {
-            await requester.SendAsync(null, HttpMethod.Delete, relativeUrl, null, token);
+            return requester.SendAsync(null, HttpMethod.Delete, relativeUrl, null, token);
         }
     }
 }
