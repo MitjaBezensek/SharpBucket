ed0b0e50ec9f5fe1f23d95ce967ea921ca3c33a7
Mickael NIVET <mnivet@talentsoft.com>
2020-03-15 00:13:51 +0100

    Add the link to go to the reported issue in BitBucket support about the unexpected change in the avatar URL of a project after an update of the description of that project.

--- SharpBucketTests/V2/Pocos/ProjectInfoAssertions.cs
+++ SharpBucketTests/V2/Pocos/ProjectInfoAssertions.cs
@@ -35,6 +35,9 @@ namespace SharpBucketTests.V2.Pocos
             return projectInfo;
         }
 
+
+        // Method created only to go around a bitbucket issue reported here:
+        // https://getsupport.atlassian.com/servicedesk/customer/portal/11/BBS-131627?error=login_required&error_description=Login+required&state=29f02f9e-f858-44c9-bc3a-9468e8db6322
         public static ProjectInfo ShouldBeEquivalentExceptAvatarTimeStampTo(this ProjectInfo projectInfo, ProjectInfo expectedProjectInfo)
         {
             if (expectedProjectInfo == null)
