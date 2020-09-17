79951e893b78601d38db20686a055e47b43399bb
Alexander Higgins <alexander.higgins@Persante.com>
2020-09-17 13:30:20 -0400

    Update Branch Restrictions POCO to include value, branch_type and branch_match_kind fields

--- SharpBucket/V2/Pocos/BranchRestriction.cs
+++ SharpBucket/V2/Pocos/BranchRestriction.cs
@@ -10,5 +10,9 @@ namespace SharpBucket.V2.Pocos
         public List<NamedLink> links { get; set; }
         public string pattern { get; set; }
         public List<User> users { get; set; }
+        public int? value { get; set; }
+        public string branch_type { get; set; }
+        public string branch_match_kind { get; set; }
+
     }
 }
