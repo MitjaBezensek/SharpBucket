702ef1262535dd54173d05560a70ea0d8b0a3b3c
Mickaël NIVET <mnivet@talentsoft.com>
2021-08-27 14:42:59 +0200

    Add async overloads in BranchingModelResource

--- SharpBucket/V2/EndPoints/BranchingModelResource.cs
+++ SharpBucket/V2/EndPoints/BranchingModelResource.cs
@@ -1,4 +1,6 @@
-﻿using SharpBucket.V2.Pocos;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using SharpBucket.V2.Pocos;
 
 namespace SharpBucket.V2.EndPoints
 {
@@ -25,6 +27,16 @@ namespace SharpBucket.V2.EndPoints
         }
 
         /// <summary>
+        /// Returns the branching model as applied to the repository. This view is read-only.
+        /// More info:
+        /// https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Bworkspace%7D/%7Brepo_slug%7D/branching-model
+        /// </summary>
+        public async Task<BranchingModel> GetBranchingModelAsync(CancellationToken token = default)
+        {
+            return await SharpBucketV2.GetAsync<BranchingModel>(BaseUrl, token);
+        }
+
+        /// <summary>
         /// Return the branching model configuration for a repository. 
         /// More info: 
         ///     https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Bworkspace%7D/%7Brepo_slug%7D/branching-model/settings#get
@@ -32,6 +44,16 @@ namespace SharpBucket.V2.EndPoints
         public BranchingModelSettings GetSettings()
         {
             return SharpBucketV2.Get<BranchingModelSettings>(BaseUrl + "/settings");
+        }
+
+        /// <summary>
+        /// Return the branching model configuration for a repository. 
+        /// More info: 
+        ///     https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Bworkspace%7D/%7Brepo_slug%7D/branching-model/settings#get
+        /// </summary>
+        public async Task<BranchingModelSettings> GetSettingsAsync(CancellationToken token = default)
+        {
+            return await SharpBucketV2.GetAsync<BranchingModelSettings>(BaseUrl + "/settings", token);
         }
 
         /// <summary>
@@ -44,5 +66,16 @@ namespace SharpBucket.V2.EndPoints
             return SharpBucketV2.Put(branchingModelSettings, BaseUrl + "/settings");
         }
 
+        /// <summary>
+        /// Update the branching model configuration for a repository.
+        /// More info: 
+        ///     https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Bworkspace%7D/%7Brepo_slug%7D/branching-model/settings#put
+        /// </summary>
+        public async Task<BranchingModelSettings> PutSettingsAsync(
+            BranchingModelSettings branchingModelSettings,
+            CancellationToken token = default)
+        {
+            return await SharpBucketV2.PutAsync(branchingModelSettings, BaseUrl + "/settings", token);
+        }
     }
 }
--- SharpBucketTests/V2/EndPoints/BranchingModelResourceTests.cs
+++ SharpBucketTests/V2/EndPoints/BranchingModelResourceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using SharpBucket.V2.Pocos;
 using SharpBucketTests.V2.Pocos;
@@ -22,10 +23,30 @@ namespace SharpBucketTests.V2.EndPoints
         }
 
         [Test]
+        public async Task GetBranchingModelAsync_NotConfiguredRepository_ReturnDefaultBranchingModel()
+        {
+            var repo = SampleRepositories.EmptyTestRepository;
+            var branchingModel = await repo.BranchingModelResource.GetBranchingModelAsync();
+            branchingModel.ShouldBeFilled();
+
+            // Also check default value for release branch type to ensure that
+            // reset after PutSettings tests are valid
+            branchingModel.branch_types.ShouldContain(p => p.kind == "release" && p.prefix == "release/");
+        }
+
+        [Test]
         public void GetSettings_NotConfiguredRepository_ReturnDefaultSettings()
         {
             var repo = SampleRepositories.EmptyTestRepository;
             var branchingModelSettings = repo.BranchingModelResource.GetSettings();
+            branchingModelSettings.ShouldBeFilled();
+        }
+
+        [Test]
+        public async Task GetSettingsAsync_NotConfiguredRepository_ReturnDefaultSettings()
+        {
+            var repo = SampleRepositories.EmptyTestRepository;
+            var branchingModelSettings = await repo.BranchingModelResource.GetSettingsAsync();
             branchingModelSettings.ShouldBeFilled();
         }
 
@@ -63,6 +84,42 @@ namespace SharpBucketTests.V2.EndPoints
                 },
             };
             repo.BranchingModelResource.PutSettings(settings);
+        }
+
+        [Test]
+        public async Task PutSettingsAsync_NotConfiguredRepository_ReturnDefaultSettings()
+        {
+            var repo = SampleRepositories.EmptyTestRepository;
+            var settings = new BranchingModelSettings
+            {
+                branch_types = new List<BranchingModelSettingsBranchType>
+                {
+                    new BranchingModelSettingsBranchType
+                    {
+                        kind = "release",
+                        prefix = "delivered/",
+                        enabled = true,
+                    },
+                },
+            };
+            var branchingModelSettings = await repo.BranchingModelResource.PutSettingsAsync(settings);
+            branchingModelSettings.ShouldBeFilled();
+            branchingModelSettings.branch_types.ShouldContain(p => p.kind == "release" && p.prefix == "delivered/");
+
+            // put back default value
+            settings = new BranchingModelSettings
+            {
+                branch_types = new List<BranchingModelSettingsBranchType>
+                {
+                    new BranchingModelSettingsBranchType
+                    {
+                        kind = "release",
+                        prefix = "release/",
+                        enabled = true,
+                    },
+                },
+            };
+            await repo.BranchingModelResource.PutSettingsAsync(settings);
         }
     }
 }
