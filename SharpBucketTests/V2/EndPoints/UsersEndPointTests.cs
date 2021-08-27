8fbf08cbdaa34fb083d6019850a7bc4032af9db5
Mickaël NIVET <mnivet@talentsoft.com>
2021-08-27 15:58:44 +0200

    Remove methods and properties that were obsolete since version 0.11.0 relative to GDPR.
    Only the usernmae property in User object has been kept, with obsolete tag removed, and comment improved.
    This is in fact still used for team users which may still appear in some query responses.
    Even if with current deprecation of teams in favor of workspaces we may thing that those users may completely disappear, they are still there for now.

--- SharpBucketTests/V2/EndPoints/UsersEndPointTests.cs
+++ SharpBucketTests/V2/EndPoints/UsersEndPointTests.cs
@@ -33,13 +33,6 @@ namespace SharpBucketTests.V2.EndPoints
             profile.display_name.ShouldBe("mirror", nameof(profile.display_name));
             profile.created_on.ShouldBe("2008-06-26T13:58:38+00:00");
             profile.account_status.ShouldBe("active", nameof(profile.account_status));
-
-            // Obsolete properties. Their values are expected to become null or day or another
-#pragma warning disable 618
-            profile.username.ShouldBeNull(nameof(profile.username) + " is no longer available due to GDPR concerns");
-            profile.website.ShouldBeNull(nameof(profile.website) + " is no longer available due to GDPR concerns");
-            profile.location.ShouldBeNull(nameof(profile.location) + " is no longer available due to GDPR concerns");
-#pragma warning restore 618
         }
 
         [Test]
@@ -55,33 +48,6 @@ namespace SharpBucketTests.V2.EndPoints
             profile.display_name.ShouldBe("mirror", nameof(profile.display_name));
             profile.created_on.ShouldBe("2008-06-26T13:58:38+00:00");
             profile.account_status.ShouldBe("active", nameof(profile.account_status));
-
-            // Obsolete properties. Their values are expected to become null or day or another
-#pragma warning disable 618
-            profile.username.ShouldBeNull(nameof(profile.username) + " is no longer available due to GDPR concerns");
-            profile.website.ShouldBeNull(nameof(profile.website) + " is no longer available due to GDPR concerns");
-            profile.location.ShouldBeNull(nameof(profile.location) + " is no longer available due to GDPR concerns");
-#pragma warning restore 618
-        }
-
-        [Test]
-        [Obsolete("Test of an obsolete method")]
-        public void ListFollowers_FromMirrorAccount_ShouldThrowExceptionWithDeprecationMessage()
-        {
-            usersEndPoint.ShouldNotBe(null);
-
-            var exception = Assert.Throws<BitbucketV2Exception>(() => usersEndPoint.ListFollowers(15));
-            exception.Message.ShouldContain("Resource not found");
-        }
-
-        [Test]
-        [Obsolete("Test of an obsolete method")]
-        public void ListFollowing_FromMirrorAccount_ShouldThrowExceptionWithDeprecationMessage()
-        {
-            usersEndPoint.ShouldNotBe(null);
-
-            var exception = Assert.Throws<BitbucketV2Exception>(() => usersEndPoint.ListFollowing());
-            exception.Message.ShouldContain("Resource not found");
         }
 
         [Test]
